f3fa481805553bbcdd05624cc32a1c855e06eba3
Levael <Levael.Savvy@gmail.com>
2023-06-25 01:22:28 +0300

    some small upd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as chart
 import numpy as np
 
 
-def GetData (command_file_path, real_file_path):
+def GetRawData (command_file_path, real_file_path):
     with open(command_file_path) as command_file, open(real_file_path) as real_file:
         # ignore first line (columns names)
         command_data = command_file.readlines()[1::]
@@ -10,7 +10,6 @@ def GetData (command_file_path, real_file_path):
 
         min_len = min(len(command_data), len(real_data))
 
-        # format: [list of command forward, list of command side, list of real forward, list of real side]
         OUTPUT_DATA_raw = [[0] * min_len for _ in range(4)]
 
         for index in range(min_len):
@@ -30,28 +29,25 @@ def GetData (command_file_path, real_file_path):
         return OUTPUT_DATA_raw
 
 
-# list of format ["command forward", "command side", "real forward", "real side"]
-MOOG_DATA_raw = GetData("Command DOF.txt", "Feedback DOF.txt")
+# format: [list of COMMAND_FORWARD, list of COMMAND_SIDE, list of REAL_FORWARD, list of REAL_SIDE]
+MOOG_DATA_raw = GetRawData("Command DOF.txt", "Feedback DOF.txt")
 
 COMMAND_FORWARD = np.array(MOOG_DATA_raw[0], dtype=float)
-COMMAND_SIDE = np.array(MOOG_DATA_raw[1], dtype=float)
-REAL_FORWARD = np.array(MOOG_DATA_raw[2], dtype=float)
-REAL_SIDE = np.array(MOOG_DATA_raw[3], dtype=float)
+COMMAND_SIDE    = np.array(MOOG_DATA_raw[1], dtype=float)
+REAL_FORWARD    = np.array(MOOG_DATA_raw[2], dtype=float)
+REAL_SIDE       = np.array(MOOG_DATA_raw[3], dtype=float)
 
-#max diff command/real
-#min value, max value
-# devide to trials
-#lens of trials
-
+# rendering
 chart.plot(COMMAND_FORWARD[1000:2500])
 chart.plot(REAL_FORWARD[1000:2500])
 chart.show()
 
-#print(MOOG_DATA[0][1000:2000])
+#TODO
+#max diff command/real
+#min value, max value
+# divide to trials
+#lens of trials
 
-# rendering
-#chart.plot(MOOG_DATA[3][2000:3000])
-#chart.show()
 
 #with open("temp_output", "w") as output_file:
 #    data = MOOG_DATA[0][1000:2000]
