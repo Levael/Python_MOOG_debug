7e4b6c3a2f351c967c2c7bae4bda7f688f7b5715
Levael <Levael.Savvy@gmail.com>
2023-08-06 17:33:56 +0300

    moog

--- moog_delays.py
+++ moog_delays.py
@@ -1,33 +1,48 @@
 import matplotlib.pyplot as chart
 import numpy as np
-import re       #regular expression
+import re
 
 
 class MoogAnalytics:
     def __init__(self, file_path):
         self.raw_data = self.GetRawData(file_path)
-        self.time_stamp, self.free_data_param = self.GetDataParsedToFloat(self.raw_data)
-        self.cpu_freq = 10_000_000
-        self.chart_to_show = 'all'
-        self.divergent_trials = []
 
     def GetRawData (self, file_path):
+        OUTPUT_DATA = []
+
         with open(file_path) as file:
-            data = file.readlines()[0].split(';')[:-1]
+            for line in file:
+                OUTPUT_DATA.append(float(line))
 
-            data_len = len(data)
+        return OUTPUT_DATA
 
-            OUTPUT_DATA = [[], []]
+    def GetDiffs (self):
+        OUTPUT_DATA = []
 
-            for index in range(data_len):
-                #print(data[index])
+        for delay in range(1, len(self.raw_data)):
+            OUTPUT_DATA.append(float(self.raw_data[delay] - self.raw_data[delay-1]))
 
-                first_parameter, second_parameter = data[index].split(',')
+        return OUTPUT_DATA
 
-                OUTPUT_DATA[0].append(first_parameter)
-                OUTPUT_DATA[1].append(second_parameter)
 
-            return OUTPUT_DATA
+    def DrawPointChart (self):
+        chart.figure('')
+        chart.subplot(1, 1, 1)
+        chart.title('Moog full movement')
+        chart.xlabel('"time"')
+        chart.ylabel('diff between "frames"')
+
+        data = self.GetDiffs()[8000:12000]
+        #data = self.raw_data[8000:12000]
+
+        #chart.scatter(range(len(data)), data, s=1)
+        chart.plot(data)
+
+        chart.show()
+
+
+
+
 
     def GetDataParsedToFloat (self, data):
         return [
@@ -67,19 +82,13 @@ class MoogAnalytics:
 
             total_time = round(sum(trial), 2)
 
-            if total_time < 500: continue
-            if total_time > 1000:
-                self.divergent_trials.append(trial)
+            if total_time < 50: continue
+            #if total_time > 100: continue
 
             delays.append(total_time)
+            print(total_time)
 
         return delays
-
-
-    def AnalyzeDivergentTrials (self):
-        self.GetTrialsTotalTimes()
-        self.DrawCharts(self.divergent_trials)
-
 
     def MakeChartAsAxes (self, data, number_of_charts = 1, index_in_figure = 1, chart_name = "", y_axis_name = ""):
         chart.subplot(1, number_of_charts, index_in_figure)
@@ -90,21 +99,19 @@ class MoogAnalytics:
         x_axis = range(1, len(data)+1)
         y_axis = data
 
-        print(data)
-
         chart.scatter(x_axis, y_axis, s=1)
 
-    def DrawChartAsAxes (self, data):
+    def DrawChartAsAxes (self):
         chart.figure('')
-        self.MakeChartAsAxes(data)
-        #chart.title('Test')
+        self.MakeChartAsAxes(data = self.GetTrialsTotalTimes())
+        chart.title('Test')
         chart.xlabel('number of trials')
         chart.ylabel('trial total time')
         chart.show()
 
 
-    def DrawCharts (self, data):
-        list_of_trials = data
+    def DrawCharts (self):
+        list_of_trials = self.GetFramesDelays()
 
         chart.figure('Charts of ' + str(len(list_of_trials)) + ' last trials')
 
@@ -120,6 +127,5 @@ class MoogAnalytics:
 
 
 
-Moog_Analytics = MoogAnalytics("C:\\Users\\user\\Desktop\\temp_debug.txt")
-Moog_Analytics.DrawChartAsAxes(Moog_Analytics.GetTrialsTotalTimes())
-#Moog_Analytics.AnalyzeDivergentTrials()
+Moog_Analytics = MoogAnalytics("C:\\Users\\user\\Desktop\\Data\\temp_python.txt")
+print(Moog_Analytics.DrawPointChart())
